5f3d8749e29f38fabab3029afc75dfa0c0e2ce21
Darby Manning <me@darbymanning.com>
2024-02-27 17:47:28 +0000

    Add export for Components type

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,7 @@ import { type Document, OpenAPIClientAxios } from "openapi-client-axios"
 import definition from "./openapi.json"
 
 import type { Client } from "./openapi.d.ts"
+export type { Components } from "./openapi.d.ts"
 
 export function init(base_url?: string) {
 	const client = new OpenAPIClientAxios({
