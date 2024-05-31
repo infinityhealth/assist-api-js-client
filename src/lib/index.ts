7e4c23baab438f90118a24fa970673d185fee6a2
Darby Manning <me@darbymanning.com>
2024-05-31 22:04:59 +0100

    Make playwright a happy bunny

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 import { err, ok } from "neverthrow"
 import { type Document, OpenAPIClientAxios } from "openapi-client-axios"
-import definition from "./openapi.json"
+import definition from "./openapi.json" assert { type: "json" }
 
 import type { Client } from "./openapi.d.ts"
 export type { Components, Paths } from "./openapi.d.ts"
