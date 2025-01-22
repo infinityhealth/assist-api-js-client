31dc24f90a4fd435da688c148f0ae4e48a1d9c62
Darby Manning <me@darbymanning.com>
2025-01-22 16:45:41 +0000

    Add better API Error typing

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -2,14 +2,21 @@ import type { AxiosError } from "axios"
 import type { AxiosResponse } from "axios"
 import type { ResultAsync } from "neverthrow"
 
-export type APIError = AxiosError<{
-	errors: {
-		code: string
-		detail: string
-		source: { pointer: string }
-		status: string
-	}[]
-}>
+export type APIError<
+	T =
+		| {
+				errors: Array<{
+					code: string
+					detail: string
+					source: { pointer: string }
+					status: string
+				}>
+		  }
+		| {
+				error: string
+				error_description: string
+		  },
+> = AxiosError<T>
 
 declare module "openapi-client-axios" {
 	export type OperationResponse<Response> = ResultAsync<AxiosResponse<Response>, APIError>
